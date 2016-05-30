4f1d30612423a024ded7d5dcab04c7a6c284e354
MichalSzarek <michal.szarek@isp.pl>
2016-05-30 23:05:06 +0200

    first change

--- src/main/java/pl/example/logRegex/logRegex.java
+++ src/main/java/pl/example/logRegex/logRegex.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public class logRegex {
 	private String log;
 	//String log = "May 18 09:31:44 10.51.177.2 mwg: [18/May/2016:09:31:44 +1000] \"pgor\" 10.52.28.227 200 \"CONNECT buttons.reddit.com:443 HTTP/1.1\" \"Forum/Bulletin Boards\" \"Unverified\" \"\" 0 0 \"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36\" \"\" \"0\" \"\" \"Global Whitelist: Sites\" \"198.41.208.137\" \"\""; 
-			
+			//test git
 	
 	private void findAllText(Matcher matcher) {
 		int count = 0;
