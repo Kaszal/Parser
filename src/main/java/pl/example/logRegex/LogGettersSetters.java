75aaf3d3dd8ef553749a15bae86b747175aa802e
MichalSzarek <michal.szarek@isp.pl>
2016-05-31 01:02:35 +0200

    zmiany zmiany

---
+++ src/main/java/pl/example/logRegex/LogGettersSetters.java
@@ -0,0 +1,164 @@
+package pl.example.logRegex;
+
+public class LogGettersSetters {
+	
+	private String sysLogTimeStamp1;
+	private String proxyIP;
+	private String product;
+	private String eventTimeStamp;
+	private String user;
+	private String sourceIP;
+	private String status;
+	private String httpMethod;
+	private String httpProtocol;
+	private String url;
+	private String httpVersion;
+	private String httpCategory;
+	private String risk;
+	private String httpContentType;
+	private String bytesIN;
+	private String bytesOUT;
+	private String httpUserAgent;
+	private String signature;
+	private String action;
+	private String blockResult;
+	private String customRuleName;
+	private String destIP;
+	
+	
+	
+	public String getSysLogTimeStamp1() {
+		return sysLogTimeStamp1;
+	}
+	public void setSysLogTimeStamp1(String sysLogTimeStamp1) {
+		this.sysLogTimeStamp1 = sysLogTimeStamp1;
+	}
+	public String getProxyIP() {
+		return proxyIP;
+	}
+	public void setProxyIP(String proxyIP) {
+		this.proxyIP = proxyIP;
+	}
+	public String getProduct() {
+		return product;
+	}
+	public void setProduct(String product) {
+		this.product = product;
+	}
+	public String getEventTimeStamp() {
+		return eventTimeStamp;
+	}
+	public void setEventTimeStamp(String eventTimeStamp) {
+		this.eventTimeStamp = eventTimeStamp;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getSourceIP() {
+		return sourceIP;
+	}
+	public void setSourceIP(String sourceIP) {
+		this.sourceIP = sourceIP;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getHttpMethod() {
+		return httpMethod;
+	}
+	public void setHttpMethod(String httpMethod) {
+		this.httpMethod = httpMethod;
+	}
+	public String getHttpProtocol() {
+		return httpProtocol;
+	}
+	public void setHttpProtocol(String httpProtocol) {
+		this.httpProtocol = httpProtocol;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getHttpVersion() {
+		return httpVersion;
+	}
+	public void setHttpVersion(String httpVersion) {
+		this.httpVersion = httpVersion;
+	}
+	public String getHttpCategory() {
+		return httpCategory;
+	}
+	public void setHttpCategory(String httpCategory) {
+		this.httpCategory = httpCategory;
+	}
+	public String getRisk() {
+		return risk;
+	}
+	public void setRisk(String risk) {
+		this.risk = risk;
+	}
+	public String getHttpContentType() {
+		return httpContentType;
+	}
+	public void setHttpContentType(String httpContentType) {
+		this.httpContentType = httpContentType;
+	}
+	public String getBytesIN() {
+		return bytesIN;
+	}
+	public void setBytesIN(String bytesIN) {
+		this.bytesIN = bytesIN;
+	}
+	public String getBytesOUT() {
+		return bytesOUT;
+	}
+	public void setBytesOUT(String bytesOUT) {
+		this.bytesOUT = bytesOUT;
+	}
+	public String getHttpUserAgent() {
+		return httpUserAgent;
+	}
+	public void setHttpUserAgent(String httpUserAgent) {
+		this.httpUserAgent = httpUserAgent;
+	}
+	public String getSignature() {
+		return signature;
+	}
+	public void setSignature(String signature) {
+		this.signature = signature;
+	}
+	public String getAction() {
+		return action;
+	}
+	public void setAction(String action) {
+		this.action = action;
+	}
+	public String getBlockResult() {
+		return blockResult;
+	}
+	public void setBlockResult(String blockResult) {
+		this.blockResult = blockResult;
+	}
+	public String getCustomRuleName() {
+		return customRuleName;
+	}
+	public void setCustomRuleName(String customRuleName) {
+		this.customRuleName = customRuleName;
+	}
+	public String getDestIP() {
+		return destIP;
+	}
+	public void setDestIP(String destIP) {
+		this.destIP = destIP;
+	}
+	
+
+}
---
+++ src/main/java/pl/example/logRegex/LogRegexSplit.java
@@ -0,0 +1,15 @@
+package pl.example.logRegex;
+
+public class LogRegexSplit {
+	
+	String log = "May 18 09:31:44 10.51.177.2 mwg: [18/May/2016:09:31:44 +1000] \"pgor\" 10.52.28.227 200 \"CONNECT buttons.reddit.com:443 HTTP/1.1\" \"Forum/Bulletin Boards\" \"Unverified\" \"\" 0 0 \"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36\" \"\" \"0\" \"\" \"Global Whitelist: Sites\" \"198.41.208.137\" \"\"";
+	String[] tmp = log.split("\\s");
+	
+	LogGettersSetters log1 = new LogGettersSetters();
+	
+	for(int i=0; i < tmp.length(); i++) {
+		log1.setSysLogTimeStamp1(tmp[i]);
+	}
+
+} 
+
